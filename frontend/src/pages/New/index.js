dac9cc863c28200d89506947cded4ed92f85658c
lucaspontoexe <ltx16@hotmail.com>
2019-10-12 16:53:42 -0300

    basic drag-and-drop support

--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -11,6 +11,17 @@ export default function New({ history }) {
   const [techs, setTechs] = useState('');
   const [price, setPrice] = useState('');
 
+  function handleDragOver(event) {
+    event.preventDefault();
+  }
+
+  function handleDrop(event) {
+    event.preventDefault();
+
+    const item = event.dataTransfer.items[0].getAsFile();
+    setThumbnail(item);
+  }
+
   const preview = useMemo(() => {
     return thumbnail ? URL.createObjectURL(thumbnail) : null;
   }, [thumbnail])
@@ -39,6 +50,8 @@ export default function New({ history }) {
         id="thumbnail" 
         style={{ backgroundImage: `url(${preview})` }}
         className={thumbnail ? 'has-thumbnail' : ''}
+        onDrop={handleDrop}
+        onDragOver={handleDragOver}
       >
         <input type="file" onChange={event => setThumbnail(event.target.files[0])} />
         <img src={camera} alt="Select img" />
